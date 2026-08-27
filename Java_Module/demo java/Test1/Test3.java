1795a53f68168d0349d3c8b807cda868db54dd70
divya-2003-zade <divyazade82@gmail.com>
2026-08-27 18:55:15 +0530

    Add files via upload

---
+++ Java_Module/demo java/Test1/Test3.java
@@ -0,0 +1,17 @@
+class PlacedStudent{
+	int frn;
+	String studentName;
+	int distanceCovered;
+	String companyname;
+	String designation;
+}
+class TestPlacedStudent{
+	public static void main(String args[])
+	{
+	System.out.println("Student Placed Data");
+	PlacedStudent s1;//reference
+	s1=new PlacedStudent();
+	System.out.println(s1);
+	}
+}
+
