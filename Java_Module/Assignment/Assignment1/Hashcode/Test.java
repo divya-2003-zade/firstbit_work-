081956c1dc25b8033febea3ec2f56bd6c983ffe0
divya-2003-zade <divyazade82@gmail.com>
2026-09-01 15:39:33 +0530

    Add files via upload

---
+++ Java_Module/Assignment/Assignment1/Hashcode/Test.java
@@ -0,0 +1,15 @@
+class Date{
+	int day;
+	int month;
+	int year;
+	String dow;
+}
+//class date end here
+class Test{
+	public static void main(String args[]){
+	System.out.println("Hello java!!");
+	Date d1;//reference
+	d1=new Date();//class ka variable
+	System.out.println(d1);
+}
+}
---
+++ Java_Module/Assignment/Assignment1/Hashcode/Test5.java
@@ -0,0 +1,15 @@
+class HR{
+	int id;
+	String name;
+	double salary;
+}
+class HRData{
+	public static void main(String args[])
+	{
+	System.out.println("HR Details");
+	HR h1;//reference
+	h1=new HR();
+	System.out.println(h1);
+	}
+}
+	
